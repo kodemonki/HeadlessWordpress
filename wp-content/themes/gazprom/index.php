4d4cad622716b35d9dea01a20325d9e7743599b6
Tom Jane <kodemonki@hotmail.com>
2018-01-29 12:00:15 +0000

    plugins

--- wp-content/themes/gazprom/index.php
+++ wp-content/themes/gazprom/index.php
@@ -1,40 +1,35 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-  <meta charset="<?php bloginfo( 'charset' ); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
 
-  <?php wp_head(); ?>
+<head>
+    <meta charset="UTF-8">
+    <title>WP Headless CMS</title>
 </head>
 
-<body <?php body_class(); ?>>
+<body>
+    <!-- the element which will hold the pages list -->
+    <div id="pages-list"></div>
 
-<div class="wrap">
-  <div id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
+    <!-- load the polyfill -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fetch/2.0.3/fetch.min.js" charset="utf-8"></script>
 
-      <?php
-      if (have_posts()) :
-        /* Start the Loop */
-        while (have_posts()) : the_post();
-      ?>
-        <div>
-          <a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
-        </div>
-      <?php
-        endwhile;
-        /* End the Loop */
-      else :
-        // Nothing
-      endif;
-      ?>
+    <script type="text/javascript">
+        const pagesContainer = document.getElementById('pages-list')
+        // fetch all pages
+        // here I'm using port 8888 on localhost, but your URL might of course be different
+        fetch('http://localhost:8000/wp-json/wp/v2/pages/')
+            // get the response as JSON
+            .then(r => r.json())
+            // go through the pages and append each pages' title to the HTML element
+            .then(pages => {
+                pages.map(page => {
+                    const pageDiv = document.createElement('div')
+                    pageDiv.innerHTML = '<h1>' + page.title.rendered + '</h1><p>' + page.content.rendered + '</p>';
+                    pagesContainer.appendChild(pageDiv)
+                })
+            })
 
-    </main><!-- #main -->
-  </div><!-- #primary -->
-</div><!-- .wrap -->
-
-<?php get_footer(); ?>
-<?php wp_footer(); ?>
-
+    </script>
 </body>
+
 </html>
