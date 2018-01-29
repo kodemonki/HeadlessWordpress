2d527854b483eda35192cb2bce19aed5d7974619
Tom Jane <kodemonki@hotmail.com>
2018-01-29 17:09:18 +0000

    blank theme

--- wp-content/themes/gazprom/index.php
+++ wp-content/themes/gazprom/index.php
@@ -8,25 +8,50 @@
 
 <body>
     <!-- the element which will hold the pages list -->
-    <div id="pages-list"></div>
 
+    <h2>Php</h2>
+    <div id="pages-list-php">
+        <?php 
+        
+        $url="http://localhost:80/wp-json/wp/v2/pages/2/";
+        //  Initiate curl        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_VERBOSE, true);
+        // Disable SSL verification
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        // Will return the response, if false it print the response
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        // Set the url
+        curl_setopt($ch, CURLOPT_URL,$url);
+        // Execute
+        $result = curl_exec($ch);
+        // Will dump a beauty json :3
+        $page = (json_decode($result, true));        
+        
+        echo '<h1>'.($page['title']['rendered']).'</h1>';
+        echo '<p>'.($page['content']['rendered']).'</p>';   
+        
+        // Closing
+        curl_close($ch);
+        ?>
+
+    </div>
+    <h2>Javascript</h2>
+    <div id="pages-list"></div>
     <!-- load the polyfill -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/fetch/2.0.3/fetch.min.js" charset="utf-8"></script>
 
     <script type="text/javascript">
         const pagesContainer = document.getElementById('pages-list')
         // fetch all pages
-        // here I'm using port 8888 on localhost, but your URL might of course be different
-        fetch('http://localhost:8000/wp-json/wp/v2/pages/')
+        fetch('http://localhost:8000/wp-json/wp/v2/pages/2/')
             // get the response as JSON
             .then(r => r.json())
             // go through the pages and append each pages' title to the HTML element
-            .then(pages => {
-                pages.map(page => {
-                    const pageDiv = document.createElement('div')
-                    pageDiv.innerHTML = '<h1>' + page.title.rendered + '</h1><p>' + page.content.rendered + '</p>';
-                    pagesContainer.appendChild(pageDiv)
-                })
+            .then(page => {
+                const pageDiv = document.createElement('div');
+                pageDiv.innerHTML = '<h1>' + page.title.rendered + '</h1><p>' + page.content.rendered + '</p>';
+                pagesContainer.appendChild(pageDiv);
             })
 
     </script>
