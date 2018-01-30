7fe49b01dd0dbfee91c02f94f331476e7e1817e7
Tom Jane <kodemonki@hotmail.com>
2018-01-30 16:08:18 +0000

    removed old themes

--- wp-content/themes/gazprom/index.php
+++ wp-content/themes/gazprom/index.php
@@ -11,28 +11,12 @@
 
     <h2>Php</h2>
     <div id="pages-list-php">
-        <?php 
+        <?php
         
-        $url="http://localhost:80/wp-json/wp/v2/pages/2/";
-        //  Initiate curl        
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_VERBOSE, true);
-        // Disable SSL verification
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        // Will return the response, if false it print the response
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        // Set the url
-        curl_setopt($ch, CURLOPT_URL,$url);
-        // Execute
-        $result = curl_exec($ch);
-        // Will dump a beauty json :3
-        $page = (json_decode($result, true));        
+        $dataObj = Url2Obj('http://localhost:80/wp-json/wp/v2/pages/2/');
         
-        echo '<h1>'.($page['title']['rendered']).'</h1>';
-        echo '<p>'.($page['content']['rendered']).'</p>';   
-        
-        // Closing
-        curl_close($ch);
+        echo '<h1>'.($dataObj['title']['rendered']).'</h1><p>'.($dataObj['content']['rendered']).'</p>';   
+                        
         ?>
 
     </div>
@@ -43,11 +27,8 @@
 
     <script type="text/javascript">
         const pagesContainer = document.getElementById('pages-list')
-        // fetch all pages
         fetch('http://localhost:8000/wp-json/wp/v2/pages/2/')
-            // get the response as JSON
             .then(r => r.json())
-            // go through the pages and append each pages' title to the HTML element
             .then(page => {
                 const pageDiv = document.createElement('div');
                 pageDiv.innerHTML = '<h1>' + page.title.rendered + '</h1><p>' + page.content.rendered + '</p>';
